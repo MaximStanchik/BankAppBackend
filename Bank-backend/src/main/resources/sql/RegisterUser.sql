28b0393b9295a304c64da08d8bd373fd1a080629
Maxim Stanchik <121092454+MaximStanchik@users.noreply.github.com>
2024-11-26 17:35:24 +0300

    Added files
    Added files

---
+++ Bank-backend/src/main/resources/sql/RegisterUser.sql
@@ -0,0 +1,26 @@
+create or replace PROCEDURE REGISTER_USER(
+    p_login IN VARCHAR2,
+    p_password IN VARCHAR2,
+    p_user_id OUT NUMBER
+) AS
+    v_user_count NUMBER;
+begin
+
+    SELECT COUNT(*)
+    INTO v_user_count
+    FROM App_User
+    WHERE LOGIN = p_login;
+
+    IF v_user_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'User already exists');
+    END IF;
+
+    IF LENGTH(p_login) < 3 OR LENGTH(p_password) < 6 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Login or password are not valid');
+    END IF;
+
+    INSERT INTO App_User (LOGIN, PASSWORD)
+    VALUES (p_login, p_password)
+    RETURNING ID INTO p_user_id;
+END;
+/
